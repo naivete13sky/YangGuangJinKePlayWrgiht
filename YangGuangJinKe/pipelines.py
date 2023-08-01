2989bcf60e4378c7be28330743afc9ff478f8a3e
naivete13sky <chen320821@163.com>
2023-08-01 16:24:29 +0800

    pipeline2

--- YangGuangJinKe/pipelines.py
+++ YangGuangJinKe/pipelines.py
@@ -5,9 +5,70 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import sqlite3
-
+import pandas as pd
 
 class YangGuangJinKePipeline(object):
+
+    def __init__(self):
+        pass
+
+    def open_spider(self, spider):
+        pass
+        self.conn = sqlite3.connect("my_scrapy3.db")
+
+
+    def close_spider(self, spider):
+        pass
+        self.conn.close()
+
+    def process_item(self, item, spider):
+        pass
+        # 创建一条记录
+        name = item['name']
+        bdh = item['bdh']
+        sfzh = item['sfzh']
+        br_mobile = item['br_mobile']
+        pe_name = item['pe_name']
+        pe_mobile = item['pe_mobile']
+        xd_name = item['xd_name']
+        xd_mobile = item['xd_mobile']
+        fq_name = item['fq_name']
+        fq_mobile = item['fq_mobile']
+        mq_name = item['mq_name']
+        mq_mobile = item['mq_mobile']
+        ts_name = item['ts_name']
+        ts_mobile = item['ts_mobile']
+        qt_name = item['qt_name']
+        qt_mobile = item['qt_mobile']
+        new = pd.DataFrame({'姓名': name,
+                            '保单号': bdh,
+                            '身体证号码': sfzh,
+                            '本人手机号': br_mobile,
+                            '配偶': pe_name,
+                            '配偶手机号': pe_mobile,
+                            '兄弟': xd_name,
+                            '兄弟手机号': xd_mobile,
+                            '父亲': fq_name,
+                            '父亲手机号': fq_mobile,
+                            '母亲': mq_name,
+                            '母亲手机号': mq_mobile,
+                            '同事': ts_name,
+                            '同事手机号': ts_mobile,
+                            '其他': qt_name,
+                            '其他手机号': qt_mobile,
+                            }, index=[1]
+                           )  # 自定义索引为：1 ，这里也可以不设置index
+
+        new.to_sql("cc", self.conn, if_exists='append', index=False)
+
+    def create_table(self):
+        pass
+
+
+
+
+
+class YangGuangJinKePipeline_sql(object):
 
     def __init__(self):
         pass
@@ -44,7 +105,7 @@ class YangGuangJinKePipeline(object):
                                         ts_mobile,
                                         qt_name,
                                         qt_mobile
-                                        
+
             )
         ''')
 
@@ -67,15 +128,12 @@ class YangGuangJinKePipeline(object):
                                         ?,?,
                                         ?,?
                                     )
-                                    ''',(item['name'], item['bdh'], item['sfzh'],item['br_mobile'],
-                           item['pe_name'],item['pe_mobile'],
-                           item['xd_name'], item['xd_mobile'],
-                           item['fq_name'], item['fq_mobile'],
-                           item['mq_name'], item['mq_mobile'],
-                           item['ts_name'], item['ts_mobile'],
-                           item['qt_name'], item['qt_mobile'])
+                                    ''', (item['name'], item['bdh'], item['sfzh'], item['br_mobile'],
+                                          item['pe_name'], item['pe_mobile'],
+                                          item['xd_name'], item['xd_mobile'],
+                                          item['fq_name'], item['fq_mobile'],
+                                          item['mq_name'], item['mq_mobile'],
+                                          item['ts_name'], item['ts_mobile'],
+                                          item['qt_name'], item['qt_mobile'])
                             )
         self.connection.commit()
-
-
-
--- YangGuangJinKe/spiders/yangGuangJinKe_spider.py
+++ YangGuangJinKe/spiders/yangGuangJinKe_spider.py
@@ -47,14 +47,19 @@ class YangGuangJinKeSpider(Spider):
             with open(r'temp\{}.html'.format(int(round(time.time() * 1000))), 'w', encoding='utf8') as f:
                 f.write(response.text)
 
+            item = YangGuangJinKeItem()
 
             soup = BeautifulSoup(response.text, 'lxml')
-            current_bdh = soup.find_all('span', class_="caseContainerLeft_tile_policyNo")[0].text
+
+            item['bdh'] = soup.find_all('span', class_="caseContainerLeft_tile_policyNo")[0].text
+
             current_name = soup.find_all('span', class_="caseContainerLeft_tile_name")[0].text
-            current_sfzh = soup.select(
+            item['name'] = current_name
+            item['sfzh'] = soup.select(
                 '#app > div > div > section > div.el-tabs.el-tabs--top > div.el-tabs__content > div.containter > div.row-content.el-row.is-justify-space-between.el-row--flex > div.el-col.el-col-18 > div > div.carousel1.el-carousel.el-carousel--horizontal > div > div.el-carousel__item.is-active.is-animating > div > div > div:nth-child(4) > div:nth-child(1) > div > span')[
                 0].text
-            # print(current_name, current_bdh, current_sfzh)
+
+
             df = pd.read_html(response.text)
             # print("df:",df)
             # my_df = df[4]
@@ -72,139 +77,91 @@ class YangGuangJinKeSpider(Spider):
             my_df = df[3]
             print('{0}的联系方式纪录数'.format(current_name), len(my_df))
             # 本人手机号
-            br_mobile = my_df[(my_df[2] == "本人") & (my_df[3] == "移动电话")].values[0][0]
-            # br_mobile = my_df[(my_df[1] == "本人") & (my_df[3] == "移动电话")].values[0][0]
+            item['br_mobile'] = my_df[(my_df[2] == "本人") & (my_df[3] == "移动电话")].values[0][0]
+            # item['br_mobile'] = my_df[(my_df[1] == "本人") & (my_df[3] == "移动电话")].values[0][0]
+
 
             # 配偶姓名
             try:
-                pe_name = my_df[(my_df[2] == "配偶") & (my_df[3] == "移动电话")].values[0][1]
-                # pe_name = my_df[(my_df[1] == "配偶") & (my_df[3] == "移动电话")].values[0][1]
+                item['pe_name'] = my_df[(my_df[2] == "配偶") & (my_df[3] == "移动电话")].values[0][1]
+                # item['pe_name']= my_df[(my_df[1] == "配偶") & (my_df[3] == "移动电话")].values[0][1]
             except:
-                pe_name = ""
+                item['pe_name'] = ""
             # 配偶手机号
             try:
-                pe_mobile = my_df[(my_df[2] == "配偶") & (my_df[3] == "移动电话")].values[0][0]
-                # pe_mobile = my_df[(my_df[1] == "配偶") & (my_df[3] == "移动电话")].values[0][0]
+                item['pe_mobile'] = my_df[(my_df[2] == "配偶") & (my_df[3] == "移动电话")].values[0][0]
+                # item['pe_mobile'] = my_df[(my_df[1] == "配偶") & (my_df[3] == "移动电话")].values[0][0]
             except:
-                pe_mobile = ""
+                item['pe_mobile'] = ""
 
             # 兄弟姓名
             try:
-                xd_name = my_df[(my_df[2] == "兄弟") & (my_df[3] == "移动电话")].values[0][1]
-                # xd_name = my_df[(my_df[1] == "兄弟") & (my_df[3] == "移动电话")].values[0][1]
+                item['xd_name'] = my_df[(my_df[2] == "兄弟") & (my_df[3] == "移动电话")].values[0][1]
+                # item['xd_name'] = my_df[(my_df[1] == "兄弟") & (my_df[3] == "移动电话")].values[0][1]
             except:
-                xd_name = ""
+                item['xd_name'] = ""
             # 兄弟手机号
             try:
-                xd_mobile = my_df[(my_df[2] == "兄弟") & (my_df[3] == "移动电话")].values[0][0]
+                item['xd_mobile'] = my_df[(my_df[2] == "兄弟") & (my_df[3] == "移动电话")].values[0][0]
                 # xd_mobile = my_df[(my_df[1] == "兄弟") & (my_df[3] == "移动电话")].values[0][0]
             except:
-                xd_mobile = ""
+                item['xd_mobile'] = ""
 
             # 父亲姓名
             try:
-                fq_name = my_df[(my_df[2] == "父亲") & (my_df[3] == "移动电话")].values[0][1]
-                # fq_name = my_df[(my_df[1] == "父亲") & (my_df[3] == "移动电话")].values[0][1]
+                item['fq_name'] = my_df[(my_df[2] == "父亲") & (my_df[3] == "移动电话")].values[0][1]
+                # item['fq_name'] = my_df[(my_df[1] == "父亲") & (my_df[3] == "移动电话")].values[0][1]
             except:
-                fq_name = ""
+                item['fq_name'] = ""
             # 父亲手机号
             try:
-                fq_mobile = my_df[(my_df[2] == "父亲") & (my_df[3] == "移动电话")].values[0][0]
-                # fq_mobile = my_df[(my_df[1] == "父亲") & (my_df[3] == "移动电话")].values[0][0]
+                item['fq_mobile'] = my_df[(my_df[2] == "父亲") & (my_df[3] == "移动电话")].values[0][0]
+                # item['fq_mobile'] = my_df[(my_df[1] == "父亲") & (my_df[3] == "移动电话")].values[0][0]
             except:
-                fq_mobile = ""
+                item['fq_mobile'] = ""
 
             # 母亲姓名
             try:
-                mq_name = my_df[(my_df[2] == "母亲") & (my_df[3] == "移动电话")].values[0][1]
-                # mq_name = my_df[(my_df[1] == "母亲") & (my_df[3] == "移动电话")].values[0][1]
+                item['mq_name'] = my_df[(my_df[2] == "母亲") & (my_df[3] == "移动电话")].values[0][1]
+                # item['mq_name'] = my_df[(my_df[1] == "母亲") & (my_df[3] == "移动电话")].values[0][1]
             except:
-                mq_name = ""
+                item['mq_name'] = ""
             # 母亲手机号
             try:
-                mq_mobile = my_df[(my_df[2] == "母亲") & (my_df[3] == "移动电话")].values[0][0]
-                # mq_mobile = my_df[(my_df[1] == "母亲") & (my_df[3] == "移动电话")].values[0][0]
+                item['mq_mobile'] = my_df[(my_df[2] == "母亲") & (my_df[3] == "移动电话")].values[0][0]
+                # item['mq_mobile'] = my_df[(my_df[1] == "母亲") & (my_df[3] == "移动电话")].values[0][0]
             except:
-                mq_mobile = ""
+                item['mq_mobile'] = ""
 
             # 同事姓名
             try:
-                ts_name = my_df[(my_df[2] == "同事") & (my_df[3] == "移动电话")].values[0][1]
-                # ts_name = my_df[(my_df[1] == "同事") & (my_df[3] == "移动电话")].values[0][1]
+                item['ts_name'] = my_df[(my_df[2] == "同事") & (my_df[3] == "移动电话")].values[0][1]
+                # item['ts_name'] = my_df[(my_df[1] == "同事") & (my_df[3] == "移动电话")].values[0][1]
             except:
-                ts_name = ""
+                item['ts_name'] = ""
             # 同事手机号
             try:
-                ts_mobile = my_df[(my_df[2] == "同事") & (my_df[3] == "移动电话")].values[0][0]
-                # ts_mobile = my_df[(my_df[1] == "同事") & (my_df[3] == "移动电话")].values[0][0]
+                item['ts_mobile'] = my_df[(my_df[2] == "同事") & (my_df[3] == "移动电话")].values[0][0]
+                # item['ts_mobile'] = my_df[(my_df[1] == "同事") & (my_df[3] == "移动电话")].values[0][0]
             except:
-                ts_mobile = ""
+                item['ts_mobile'] = ""
 
             # 其他姓名
             try:
-                qt_name = my_df[(my_df[2] == "其他") & (my_df[3] == "移动电话")].values[0][1]
-                # qt_name = my_df[(my_df[1] == "其他") & (my_df[3] == "移动电话")].values[0][1]
+                item['qt_name'] = my_df[(my_df[2] == "其他") & (my_df[3] == "移动电话")].values[0][1]
+                # item['qt_name'] = my_df[(my_df[1] == "其他") & (my_df[3] == "移动电话")].values[0][1]
             except:
-                qt_name = ""
+                item['qt_name'] = ""
             # 其他手机号
             try:
-                qt_mobile = my_df[(my_df[2] == "其他") & (my_df[3] == "移动电话")].values[0][0]
-                # qt_mobile = my_df[(my_df[1] == "其他") & (my_df[3] == "移动电话")].values[0][0]
+                item['qt_mobile'] = my_df[(my_df[2] == "其他") & (my_df[3] == "移动电话")].values[0][0]
+                # item['qt_mobile'] = my_df[(my_df[1] == "其他") & (my_df[3] == "移动电话")].values[0][0]
             except:
-                qt_mobile = ""
-
-
-
-            # 创建一条记录
-            new = pd.DataFrame({'姓名': current_name,
-                                '保单号': current_bdh,
-                                '身体证号码': current_sfzh,
-                                '本人手机号': br_mobile,
-                                '配偶': pe_name,
-                                '配偶手机号': pe_mobile,
-                                '兄弟': xd_name,
-                                '兄弟手机号': xd_mobile,
-                                '父亲': fq_name,
-                                '父亲手机号': fq_mobile,
-                                '母亲': mq_name,
-                                '母亲手机号': mq_mobile,
-                                '同事': ts_name,
-                                '同事手机号': ts_mobile,
-                                '其他': qt_name,
-                                '其他手机号': qt_mobile,
-                                }, index=[1]
-                               )  # 自定义索引为：1 ，这里也可以不设置index
-
-            print(new.values[0])
-            conn = sqlite3.connect("my_scrapy3.db")
-            new.to_sql("cc", conn, if_exists='append', index=False)
-            item=YangGuangJinKeItem()
-            item['name']=current_name
-            item['bdh'] = current_bdh
-            item['sfzh'] = current_sfzh
-            item['br_mobile'] = br_mobile
-
-            item['pe_name'] = pe_name
-            item['pe_mobile'] = pe_mobile
-
-            item['xd_name'] = xd_name
-            item['xd_mobile'] = xd_mobile
-
-            item['fq_name'] = fq_name
-            item['fq_mobile'] = fq_mobile
-
-            item['mq_name'] = mq_name
-            item['mq_mobile'] = mq_mobile
-
-            item['ts_name'] = ts_name
-            item['ts_mobile'] = ts_mobile
-
-            item['qt_name'] = qt_name
-            item['qt_mobile'] = qt_mobile
+                item['qt_mobile'] = ""
 
             yield item
 
-            print("*"*30,"current finish","*"*30)
+
 
 
 
